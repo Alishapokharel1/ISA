55a593f28c35101e7186a4f1759175062a44e7df
Alisha Pokharel <119913387+Alishapokharel1@users.noreply.github.com>
2023-03-21 15:02:39 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// var sum = function (e, f){
+//     console.log(e + f);
+// }
+// sum(5 , 6)
+// var A  = 5;
+// let a = 5 + 6;
+// console.log(a);
+// console.log(A);
+// let b = 6;
+// b = b + 10;
+// alert(b)
+// document.write("<br>Hello" + "<p>My name is Rojan.</p>");
+// const c = 10;
+// alert(c);
+// var sum = function (e, f){
+//     console.log(e + f);
+// }
+// Sum(5, 6 );
+let str1 = "Hello";
+let str2 = "Universe";
+let result = str1 + " " + str2;
+console.log(result);
+// var Sum = (e, f) => {
+//     return e + f;
+// }
+// var c = Sum(5, 6);
+// console.log(c);
+
+let continent = "asia";
+console
